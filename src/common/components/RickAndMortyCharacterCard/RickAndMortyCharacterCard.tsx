cfb1d5fa6ac7fcc296b158882c3c24f9d2afc292
Julio Cesar <juliocesar@Julios-MacBook-Pro.local>
2023-11-23 22:20:54 -0300

    feat: character status list with lateral buttons created

--- src/common/components/RickAndMortyCharacterCard/RickAndMortyCharacterCard.tsx
+++ src/common/components/RickAndMortyCharacterCard/RickAndMortyCharacterCard.tsx
@@ -1,4 +1,5 @@
-import { CharImage, CharName, ImageContainer, MainContainer } from "./styles"
+import { useRef } from "react";
+import { CharImage, CharName, CharPropertiesList, CharPropertiesListItemContainer, CharPropertiesListItemContent, CharPropertiesListItemName, ImageContainer, LeftListButton, ListContainer, MainContainer, RightListButton } from "./styles"
 
 interface IRickAndMortyCharactersCard {
     charImageUrl: string
@@ -6,6 +7,30 @@ interface IRickAndMortyCharactersCard {
 
 
 export const RickAndMortyCharacterCard : React.FC<IRickAndMortyCharactersCard> = ({charImageUrl}) => {
+    
+    const mockItem = {
+        name: "Aqua Morty",
+        status: "Alive",
+        species: "Humanoid",
+        gender: "Male",
+        image: "https://rickandmortyapi.com/api/character/avatar/21.jpeg"
+    }
+
+    const listRef = useRef<HTMLUListElement | null>(null);
+
+    const scrollListToLeft = () => {
+        if (listRef.current) {
+            listRef.current.scrollLeft -= 100; 
+        }
+    };
+
+    const scrollListToRight = () => {
+        if (listRef.current) {
+            listRef.current.scrollLeft += 100; 
+        }
+    };
+
+
     return(
         <>
         <MainContainer>
@@ -13,8 +38,26 @@ export const RickAndMortyCharacterCard : React.FC<IRickAndMortyCharactersCard> =
                 <CharImage src={charImageUrl}/>
             </ImageContainer>
             <CharName>Rick Sanchez</CharName>
-           
+            <ListContainer>
+            <LeftListButton onClick={scrollListToLeft} />
+            <CharPropertiesList ref={listRef}> 
+                <CharPropertiesListItemContainer>
+                    <CharPropertiesListItemName>Status</CharPropertiesListItemName>
+                    <CharPropertiesListItemContent>{mockItem.status}</CharPropertiesListItemContent>
+                </CharPropertiesListItemContainer>
 
+                <CharPropertiesListItemContainer>
+                    <CharPropertiesListItemName>Species</CharPropertiesListItemName>
+                    <CharPropertiesListItemContent>{mockItem.species}</CharPropertiesListItemContent>
+                </CharPropertiesListItemContainer>
+
+                <CharPropertiesListItemContainer>
+                    <CharPropertiesListItemName>Gender</CharPropertiesListItemName>
+                    <CharPropertiesListItemContent>{mockItem.gender}</CharPropertiesListItemContent>
+                </CharPropertiesListItemContainer>
+            </CharPropertiesList>
+            <RightListButton onClick={scrollListToRight}/>
+            </ListContainer>
         </MainContainer>
         </>
     )
--- src/common/components/RickAndMortyCharacterCard/styles.ts
+++ src/common/components/RickAndMortyCharacterCard/styles.ts
@@ -1,13 +1,16 @@
 import styled from "styled-components";
+import LeftArrowIcon from "../../../assets/icons/icon_left_arrow.png"
+import RightArrowIcon from "../../../assets/icons/icon_right_arrow.png"
+
 
 export const MainContainer = styled.div`
     width: 250px;
-    height: 300px;
+    height: 350px;
     display: flex;
     flex-direction: column;
     border-radius: 30px;
     background-color: #35c9dd;
-    border: 4px solid #17ff39;
+    border: 4px solid #231942;
 
     @media (max-width: 600px) {
         width: 200px;
@@ -30,7 +33,6 @@ export const CharImage = styled.img`
         height: 180px;
         display: flex;
         border-radius: 90px;
-    
 
         @media (max-width: 600px) {
             width: 150px;
@@ -42,8 +44,77 @@ export const CharImage = styled.img`
 
 export const CharName = styled.text`
     @import url('https://fonts.googleapis.com/css2?family=Creepster&family=Josefin+Sans&family=Poppins&display=swap');
+    height: fit-content;
     font-size: 30px;
     font-family: 'Creepster', cursive;
     display: flex;
     align-self: center;
+    color: #B8336A;
+   
 `
+
+export const CharPropertiesList = styled.ul`
+    max-width: 100%; 
+    margin-top: 0px;
+    display: flex;
+    padding: 0;
+    margin: 0;
+    overflow-x: hidden;
+    list-style: none;
+    padding-left: 5px; 
+`
+
+export const ListContainer = styled.div`
+    width: 230px;
+    height: fit-content;
+    display: flex;
+    align-self: center;
+    flex-direction: row;
+    background-color: #8A89C0;
+    
+    border-radius: 15px;
+`     
+
+export const CharPropertiesListItemContainer = styled.text`
+    width: fit-content;
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    white-space: nowrap;
+    margin-right: 25px;
+`
+
+export const CharPropertiesListItemName = styled.text`
+    font-size: 20px;
+    color: #17FF39;
+`
+
+export const CharPropertiesListItemContent = styled.text`
+    font-size: 20px;
+    color: #17FF39;
+`
+export const LeftListButton = styled.button`
+    width: 30px;
+    height: 30px;
+    margin-top: 10px;
+    margin-left: 5px;
+    border: none;
+    display: flex;
+    background-image: url(${LeftArrowIcon});
+    background-size: contain; /* Ajuste conforme necessário */
+    background-repeat: no-repeat;
+    background-color: transparent;
+`
+
+export const RightListButton = styled.button`
+    width: 30px;
+    height: 30px;
+    margin-top: 10px;
+    margin-right: 5px;
+    border: none;
+    display: flex;
+    background-image: url(${RightArrowIcon});
+    background-size: contain; /* Ajuste conforme necessário */
+    background-repeat: no-repeat;
+    background-color: transparent;
+`;
